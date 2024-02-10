03dc76ea71af80dae1210257f13e9ae817cdcdfa
Mohith Reddy Vuyyuru <mohithreddy2002@gmail.com>
2024-02-10 18:02:13 -0500

    Added CORS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 
 app.use('/', express.static('public'));
+app.use(cors())
 
 const budget = [];
 
